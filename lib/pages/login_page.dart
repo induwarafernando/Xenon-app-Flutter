6b18eb812af5abfbfcc3b324ba3a471512e55a3e
induwarafernando <induwarafernando9@gmail.com>
2024-02-12 13:50:05 +0530

    dark mode

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,70 +4,78 @@ import 'package:xenon_app/components/my_textfield.dart';
 import 'package:xenon_app/components/square_tile.dart';
 import 'package:xenon_app/pages/register_page.dart';
 
-//Login Page stateless widget
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-  // text editing controllers
+class _LoginPageState extends State<LoginPage> {
+  bool isDarkMode = false;
+
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
-  // sign user in method
-  //sign in button to direct to home_page
   void signUserIn() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      backgroundColor: isDarkMode ? Colors.grey[900] : Colors.white,
+      appBar: AppBar(
+        backgroundColor: isDarkMode ? Colors.grey[900] : Colors.white,
+        actions: [
+          IconButton(
+            icon: Icon(
+              isDarkMode ? Icons.light_mode : Icons.dark_mode,
+              color: isDarkMode ? Colors.white : Colors.black,
+            ),
+            onPressed: () {
+              setState(() {
+                isDarkMode = !isDarkMode;
+              });
+            },
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(height: 50),
-
-              // logo IMAGE
               Image.asset(
                 'lib/images/LogoX2.png',
                 height: 70,
                 width: 500,
               ),
-
               const SizedBox(height: 50),
-
-              // welcome back, you've been missed!
               Text(
                 'Your Styles, Your Stores, We Deliver!',
                 style: TextStyle(
-                  color: Color.fromARGB(255, 2, 35, 105),
+                  color: isDarkMode
+                      ? Colors.white
+                      : Color.fromARGB(255, 2, 35, 105),
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-
               const SizedBox(height: 25),
-
-              // username textfield
               MyTextField(
                 controller: usernameController,
                 hintText: 'Username',
                 obscureText: false,
+                isDarkMode: isDarkMode,
               ),
-
               const SizedBox(height: 10),
-
-              // password textfield
               MyTextField(
                 controller: passwordController,
                 hintText: 'Password',
                 obscureText: true,
+                isDarkMode: isDarkMode,
               ),
-
               const SizedBox(height: 10),
-
-              // forgot password?
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
@@ -75,23 +83,18 @@ class LoginPage extends StatelessWidget {
                   children: [
                     Text(
                       'Forgot Password?',
-                      style: TextStyle(color: Colors.grey[600]),
+                      style: TextStyle(
+                          color: isDarkMode ? Colors.white : Colors.grey[600]),
                     ),
                   ],
                 ),
               ),
-
               const SizedBox(height: 25),
-
-              // sign in button
-
               MyButton(
                 onTap: signUserIn,
+                isDarkMode: isDarkMode,
               ),
-
               const SizedBox(height: 10),
-
-              // or continue with
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
@@ -99,70 +102,61 @@ class LoginPage extends StatelessWidget {
                     Expanded(
                       child: Divider(
                         thickness: 0.5,
-                        color: Colors.grey[400],
+                        color: isDarkMode ? Colors.white : Colors.grey[400],
                       ),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 10.0),
                       child: Text(
                         'Or continue with',
-                        style: TextStyle(color: Colors.grey[700]),
+                        style: TextStyle(
+                            color:
+                                isDarkMode ? Colors.white : Colors.grey[700]),
                       ),
                     ),
                     Expanded(
                       child: Divider(
                         thickness: 0.5,
-                        color: Colors.grey[400],
+                        color: isDarkMode ? Colors.white : Colors.grey[400],
                       ),
                     ),
                   ],
                 ),
               ),
-
               const SizedBox(height: 10),
-
-              // google + apple sign in buttons
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
-                  // google button
                   SquareTile(
                       imagePath: 'lib/images/google.png',
                       height: 40,
                       width: 40),
-
                   SizedBox(width: 25),
-
-                  // apple button
                   SquareTile(
                       imagePath: 'lib/images/Apple.png', height: 40, width: 40)
                 ],
               ),
-
               const SizedBox(height: 10),
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
                     'Not a member?',
-                    style: TextStyle(color: Colors.grey[700]),
+                    style: TextStyle(
+                        color: isDarkMode ? Colors.white : Colors.grey[700]),
                   ),
                   const SizedBox(width: 4),
-                  // "Register now" text with onTap handler to navigate to register page
                   GestureDetector(
                     onTap: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                RegisterPage()), // Navigate to register page
+                        MaterialPageRoute(builder: (context) => RegisterPage()),
                       );
                     },
                     child: Text(
                       'Register now',
                       style: TextStyle(
-                        color: Colors.blue,
+                        color: isDarkMode ? Colors.blue : Colors.grey[700],
                         fontWeight: FontWeight.bold,
                       ),
                     ),
