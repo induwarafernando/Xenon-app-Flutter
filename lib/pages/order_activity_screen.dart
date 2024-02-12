16d80563dfe389e569798f60fc4a353e8fe48b02
induwarafernando <induwarafernando9@gmail.com>
2024-02-12 13:50:33 +0530

    update activity

--- lib/pages/order_activity_screen.dart
+++ lib/pages/order_activity_screen.dart
@@ -7,21 +7,42 @@ class OrderActivityScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // Mock list of past orders
     List<Order> pastOrders = [
-      Order(orderNumber: '01', isOutForDelivery: true),
-      Order(orderNumber: '02', isOutForDelivery: false),
-      Order(orderNumber: '03', isOutForDelivery: true),
+      Order(orderNumber: '01', orderStatus: OrderStatus.Pending),
+      Order(orderNumber: '02', orderStatus: OrderStatus.OutForDelivery),
+      Order(orderNumber: '03', orderStatus: OrderStatus.Recieved),
       // Add more orders here as needed
     ];
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Order Details'),
+        backgroundColor: Color.fromARGB(255, 238, 245, 253),
+        automaticallyImplyLeading: false, // Remove back button
       ),
-      body: ListView.builder(
-        itemCount: pastOrders.length,
-        itemBuilder: (context, index) {
-          return OrderTile(order: pastOrders[index]);
-        },
+      body: Container(
+        color:
+            Color.fromARGB(255, 238, 245, 253), // Set background color to grey
+        child: ListView.builder(
+          itemCount: pastOrders.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Card(
+                color: Color.fromARGB(
+                    184, 156, 222, 241), // Set card background color to white
+                elevation: 3, // Add a slight elevation to the card
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // Set border radius
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: OrderTile(order: pastOrders[index]),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
@@ -34,14 +55,44 @@ class OrderTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String statusText = '';
+
+    switch (order.orderStatus) {
+      case OrderStatus.Pending:
+        statusText = 'Pending';
+        break;
+      case OrderStatus.OutForDelivery:
+        statusText = 'Out for Delivery';
+        break;
+      case OrderStatus.Recieved:
+        statusText = 'Received';
+        break;
+    }
+
+    Color statusColor = Colors.black;
+
+    switch (order.orderStatus) {
+      case OrderStatus.Pending:
+        statusColor = Colors.red;
+        break;
+      case OrderStatus.OutForDelivery:
+        statusColor = Colors.blue;
+        break;
+      case OrderStatus.Recieved:
+        statusColor = Color.fromARGB(255, 2, 103, 5);
+        break;
+    }
+
     return ListTile(
       title: Text('Order number: ${order.orderNumber}'),
       subtitle: Text(
-        'Status: ${order.isOutForDelivery == true ? "Out for Delivery" : "Pending"}',
+        'Status: $statusText',
+        style: TextStyle(color: statusColor),
       ),
       leading: CircleAvatar(
+        backgroundColor: Colors.white,
         // Use a different icon or image for the leading widget if needed
-        child: Icon(order.isOutForDelivery == true
+        child: Icon(order.orderStatus == OrderStatus.OutForDelivery
             ? Icons.local_shipping
             : Icons.pending),
       ),
@@ -51,10 +102,16 @@ class OrderTile extends StatelessWidget {
 
 class Order {
   final String orderNumber;
-  final bool isOutForDelivery;
+  final OrderStatus orderStatus;
 
   const Order({
     required this.orderNumber,
-    required this.isOutForDelivery,
+    required this.orderStatus,
   });
+}
+
+enum OrderStatus {
+  Pending,
+  OutForDelivery,
+  Recieved,
 }
